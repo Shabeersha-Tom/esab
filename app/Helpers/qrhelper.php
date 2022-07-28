89ee019a658ac5865d1902d188560e54252388f4
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2022-07-28 15:56:57 +0400

    Qr ongoing

--- app/Helpers/qrhelper.php
+++ app/Helpers/qrhelper.php
@@ -7,53 +7,47 @@ use Illuminate\Support\Facades\URL;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Intervention\Image\ImageManager;
 use setasign\Fpdi\Fpdi;
-use SVG\SVG;
 
-function processImage()
-{
-    // $pdf = new Fpdi();
-    // $pdf->AddPage();
-    // $pdf->setSourceFile(public_path() . '/storage/certificates/certificate.pdf');
-    // $tplId = $pdf->importPage(1);
-    // $pdf->useTemplate($tplId);
-    // $pdf->Image(public_path() . '/admin_assests/img/logo.png', 0, 0, 0, 0);
-    // dd($pdf->Output());
-
-    $file = CertificateFile::find(1);
-    $certificate = Certificate::find(1);
-    $qrString  = generateQRCode($certificate->slug);
-
-
-    $time = time();
-    $output_file = '/public/temp/qr-code/img-' . $time . '.png';
-    Storage::disk('local')->put($output_file, $qrString  );
-
-    // $file = SVG::fromFile(storage_path('app/' . $output_file));
-
-    // $output_file = '/public/qr-code/img-' . $time . '.png';
-    // $image = Image::make(storage_path('app/' . $output_file));
-    // dd($image);
-    // Storage::disk('local')->put($output_file, $image);
-
-    // dd($image);
-
-    // $cerificate_image = $file->getFile($certificate->certificate_no);
-    // $merged_image = mergeImages($qr, $cerificate_image, $file->path);
-    // dd($merged_image);
-}
-// function processImage(Certificate $certificate, CertificateFile $file)
+// function processImage()
 // {
-//     $qr = generateQRCode($certificate->slug);
-//     $cerificate_image = $file->getFile($certificate->certificate_no);
-//     $merged_image = mergeImages($qr, $cerificate_image, $file->path);
-//     dd($merged_image);
-// }
+//     // $pdf = new Fpdi();
+//     // $pdf->AddPage();
+//     // $pdf->setSourceFile(public_path() . '/storage/certificates/certificate.pdf');
+//     // $tplId = $pdf->importPage(1);
+//     // $pdf->useTemplate($tplId);
+//     // $pdf->Image(public_path() . '/admin_assests/img/logo.png', 0, 0, 0, 0);
+//     // dd($pdf->Output());
 
-function generateQRCode($string)
+
+
+//     
+//     // $image = Image::make(storage_path('app/' . $output_file));
+//     // dd($image);
+//     // Storage::disk('local')->put($output_file, $image);
+
+//     // dd($image);
+
+//     // $cerificate_image = $file->getFile($certificate->certificate_no);
+//     // $merged_image = mergeImages($qr, $cerificate_image, $file->path);
+//     // dd($merged_image);
+// }
+function processImage(Certificate $certificate, CertificateFile $file)
+{
+    $time = time();
+    $output_file = '/public/qr-code/img-' . $time . '.png';
+    $qr = generateQRCode($certificate->slug, $time);
+    $cerificate_image = $file->getFile($certificate->certificate_no);
+    $merged_image = mergeImages($qr, $cerificate_image, $file->path);
+    dd($merged_image);
+}
+
+function generateQRCode($string, $time)
 {
     $certificate = Certificate::whereSlug($string)->firstOrFail();
     $url = URL::to(route('certificate.view', $string));
-    return QrCode::size(300)->format('png')->generate($url);
+    $qrString = QrCode::size(300)->format('png')->generate($url);
+
+    Storage::disk('local')->put($output_file, $qrString);
 }
 
 function mergeImages($qr, $cerificate, $name)
