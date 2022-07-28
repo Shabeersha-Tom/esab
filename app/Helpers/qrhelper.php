fe6d1b0e82e59244ba738a33f1b5ab8a515a8964
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2022-07-28 15:16:26 +0400

    qr test

--- app/Helpers/qrhelper.php
+++ app/Helpers/qrhelper.php
@@ -2,14 +2,15 @@
 
 use App\Models\Certificates\Certificate;
 use App\Models\Certificates\CertificateFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 use Intervention\Image\ImageManager;
 use setasign\Fpdi\Fpdi;
+use SVG\SVG;
 
 function processImage()
 {
-
     // $pdf = new Fpdi();
     // $pdf->AddPage();
     // $pdf->setSourceFile(public_path() . '/storage/certificates/certificate.pdf');
@@ -18,15 +19,27 @@ function processImage()
     // $pdf->Image(public_path() . '/admin_assests/img/logo.png', 0, 0, 0, 0);
     // dd($pdf->Output());
 
-
-
     $file = CertificateFile::find(1);
     $certificate = Certificate::find(1);
-    $qr = generateQRCode($certificate->slug);
-    dd($qr);
-    $cerificate_image = $file->getFile($certificate->certificate_no);
-    $merged_image = mergeImages($qr, $cerificate_image, $file->path);
-    dd($merged_image);
+    $qrString  = generateQRCode($certificate->slug);
+
+
+    $time = time();
+    $output_file = '/public/temp/qr-code/img-' . $time . '.svg';
+    Storage::disk('local')->put($output_file, $qrString  );
+
+    // $file = SVG::fromFile(storage_path('app/' . $output_file));
+
+    // $output_file = '/public/qr-code/img-' . $time . '.png';
+    // $image = Image::make(storage_path('app/' . $output_file));
+    // dd($image);
+    // Storage::disk('local')->put($output_file, $image);
+
+    // dd($image);
+
+    // $cerificate_image = $file->getFile($certificate->certificate_no);
+    // $merged_image = mergeImages($qr, $cerificate_image, $file->path);
+    // dd($merged_image);
 }
 // function processImage(Certificate $certificate, CertificateFile $file)
 // {
@@ -40,7 +53,7 @@ function generateQRCode($string)
 {
     $certificate = Certificate::whereSlug($string)->firstOrFail();
     $url = URL::to(route('certificate.view', $string));
-    return QrCode::size(300)->generate($url);
+    return QrCode::size(300)->format('png')->generate($url);
 }
 
 function mergeImages($qr, $cerificate, $name)
