d7049d7918920dee843f770135a255c276330e8e
= <ashikmuhammed.1729@gmail.com>
2022-08-03 04:15:51 -0700

    qr gen 4

--- app/Helpers/qrhelper.php
+++ app/Helpers/qrhelper.php
@@ -34,8 +34,6 @@ function processImage(Certificate $certificate, CertificateFile $file)
 {
     $time = time();
     $qr = generateQRCode($certificate->slug, $time);
-    dd($qr);
-    dd($qr);
     $cerificate_image = $file->getFile($certificate->certificate_no);
     $merged_image = mergeImages($qr, $cerificate_image, $file->path);
     dd($merged_image);
@@ -43,12 +41,12 @@ function processImage(Certificate $certificate, CertificateFile $file)
 
 function generateQRCode($slug, $time)
 {
-    $output_file = '/storage/qr-code/img-' . $time . '.png';
+    $output_file = '/qr-code/img-' . $time . '.png';
     // $certificate = Certificate::whereSlug($string)->firstOrFail();
     $url = URL::to(route('certificate.view', $slug));
     $qrString = QrCode::size(300)->format('png')->generate($url);
-    Storage::disk('local')->put($output_file, $qrString);
-    return URL::to($output_file);
+    Storage::disk('public')->put($output_file, $qrString);
+    return asset('storage'.$output_file );
 }
 
 function mergeImages($qr, $cerificate, $name)
