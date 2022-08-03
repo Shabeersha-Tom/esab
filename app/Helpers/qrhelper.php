79c7b21b00fc1a761108b50b2924fb9e1f7760ca
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2022-08-03 12:13:34 +0400

    qr gen 2

--- app/Helpers/qrhelper.php
+++ app/Helpers/qrhelper.php
@@ -33,21 +33,21 @@ use setasign\Fpdi\Fpdi;
 function processImage(Certificate $certificate, CertificateFile $file)
 {
     $time = time();
-    $output_file = '/public/qr-code/img-' . $time . '.png';
+    $qr = generateQRCode($certificate->slug, $time);
 
-    $qr = generateQRCode($certificate->slug, $output_file);
     $cerificate_image = $file->getFile($certificate->certificate_no);
     $merged_image = mergeImages($qr, $cerificate_image, $file->path);
     dd($merged_image);
 }
 
-function generateQRCode($string, $output_file)
+function generateQRCode($slug, $time)
 {
-    $certificate = Certificate::whereSlug($string)->firstOrFail();
-    $url = URL::to(route('certificate.view', $string));
+    $output_file = '/public/qr-code/img-' . $time . '.png';
+    // $certificate = Certificate::whereSlug($string)->firstOrFail();
+    $url = URL::to(route('certificate.view', $slug));
     $qrString = QrCode::size(300)->format('png')->generate($url);
-
-    Storage::disk('local')->put($output_file, $qrString);
+    dd($qrString);
+    return Storage::disk('local')->put($output_file, $qrString);
 }
 
 function mergeImages($qr, $cerificate, $name)
